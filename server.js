e77fa21dd4d59e5b9c62e7c33523cb67175e9b5e
JustinSHong <jhong2009@gmail.com>
2018-12-14 19:40:59 -0800

    set up server file to render static files

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const server = express();
+const helmet = require("helmet");
+const cors = require("cors");
+
+const PORT = process.env.PORT || 8081;
+
+// middleware
+server.use(helmet());
+server.use(express.json());
+server.use(cors());
+
+// render static files
+server.use(express.static(__dirname + "/dist"));
+// app.use(express.static(__dirname + "/src"));
+
+server.listen(PORT, () => {
+    console.log(`\n===API Running on port ${PORT}===\n`);
+});
