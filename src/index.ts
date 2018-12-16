8ad6db04cda6c0bcbcfdf83846989f0f68e6837c
JustinSHong <jhong2009@gmail.com>
2018-12-15 19:04:09 -0800

    index.ts: added event listener with cb to transition keyboard keys and play corresponding sounds

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,19 @@
 import "./styles.less";
+
+(function() {
+    // plays a key's corresponding sound
+    function playDrumSound(e: any) {
+        const audio: any = document.querySelector(
+            `audio[data-key="${e.keyCode}"]`
+        );
+        const key: any = document.querySelector(`div[data-key="${e.keyCode}"]`);
+        // only play sound if key codes match
+        if (audio) {
+            key.classList.add("playing");
+            audio.currentTime = 0;
+            audio.play();
+        }
+    }
+
+    window.addEventListener("keydown", playDrumSound);
+})();
